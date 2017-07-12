ff6172377db374be4d09e7b5d19b6c40e61ec809
Frederick Morlock <FrederickGeek8@gmail.com>
2017-07-12 00:28:52 -0400

    Add fadeIn
    Will implement a more monolithic animate function in the future

--- dist/monolith.js
+++ dist/monolith.js
@@ -372,4 +372,39 @@ $.each({
   };
 });
 
+Monolith.extend({
+  fadeIn: function() {
+    var tick = function(obj, state) {
+      state.opacity += (new Date() - state.last) / 400;
+      obj.style.opacity = state.opacity;
+      obj.style.filter = 'alpha(opacity=' + (100 * state.opacity) | 0 + ')';
+
+      state.last = +new Date();
+
+      var nexttick = function() {
+        tick(obj, state);
+      };
+
+      if (state.opacity < 1) {
+        if(!(window.requestAnimationFrame && requestAnimationFrame(nexttick))){
+          setTimeout(nexttick, 16);
+        }
+      }
+    };
+
+    for (var i = 0; i < this.length; i++) {
+      var state = {
+        opacity: 0,
+        last: +new Date()
+      };
+      this[i].style.opacity = 0;
+      this[i].style.filter = '';
+
+
+      tick(this[i], state);
+    }
+
+  }
+});
+
 })(window);
